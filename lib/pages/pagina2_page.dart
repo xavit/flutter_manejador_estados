4313c4c2b05706e29c51682af8ac1dd48953bce7
Javier Claros <javiclaros@gmail.com>
2022-09-29 09:09:22 -0400

    SnackBar

--- lib/pages/pagina2_page.dart
+++ lib/pages/pagina2_page.dart
@@ -13,17 +13,26 @@ class Pagina2Page extends StatelessWidget {
         children: [
           MaterialButton(
               color: Colors.blueAccent,
-              onPressed: () => print("click"),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Usuario Establecido')));
+              },
               child: const Text('Establecer Usuario',
                   style: TextStyle(color: Colors.white))),
           MaterialButton(
               color: Colors.blueAccent,
-              onPressed: () => print("click"),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Edad Modificada')));
+              },
               child: const Text('Cambiar Edad',
                   style: TextStyle(color: Colors.white))),
           MaterialButton(
               color: Colors.blueAccent,
-              onPressed: () => print("click"),
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Profesión Añañida')));
+              },
               child: const Text('Añadir Profesión',
                   style: TextStyle(color: Colors.white))),
         ],
