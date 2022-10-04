7b9f18b21a6d59b1618e24e74e250a1b1f6b42ba
Javier Claros <javiclaros@gmail.com>
2022-10-04 14:36:40 -0400

    Main Corregido

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_singleton/bloc/user/user_bloc.dart';
 import 'package:flutter_singleton/pages/pagina1_page.dart';
 import 'package:flutter_singleton/pages/pagina2_page.dart';
 
@@ -9,19 +7,14 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (_) => UserBloc()),
-      ],
-      child: MaterialApp(
-        title: 'Material App',
-        debugShowCheckedModeBanner: false,
-        initialRoute: 'pagina1',
-        routes: {
-          'pagina1': (_) => Pagina1Page(),
-          'pagina2': (_) => Pagina2Page(),
-        },
-      ),
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'pagina1',
+      routes: {
+        'pagina1': (_) => Pagina1Page(),
+        'pagina2': (_) => Pagina2Page(),
+      },
     );
   }
 }
